55957a53127eadf86d475937219318c51e907629
Pranav Gupta <guptapranav360@gmail.com>
2017-09-06 21:51:25 -0700

    PHP File Initate

--- app/src/main/java/com/medical/medicate/Register.java
+++ app/src/main/java/com/medical/medicate/Register.java
@@ -10,6 +10,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -50,5 +57,29 @@ public class Register extends AppCompatActivity
         Log.d("Email",reg_Email_value);
         Log.d("Password",reg_Password_value);
         Log.d("Confirm Passowrd",reg_ConfirmPassword_value);*/
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>()
+                    {
+                        @Override
+                        public void onResponse(String response)
+                        {
+                            // Display the first 500 characters of the response string.
+                            Log.e("Response is: ",response.substring(0,500));
+                        }
+                    }, new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error)
+                        {
+                            Log.e("Error : ","That didn't work!");
+                        }
+                    });
+        queue.add(stringRequest);
     }
 }
