a43b935233f597ba2c3fbc6b1a2bd65e52314528
Nitin Verma <nitin.spcl@gmail.com>
2017-09-15 21:53:57 +0530

    login and regestration form

--- app/src/main/java/com/medical/medicate/Login.java
+++ app/src/main/java/com/medical/medicate/Login.java
@@ -1,70 +1,127 @@
 package com.medical.medicate;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Login extends AppCompatActivity
-{
+//import android.support.design.widget.Snackbar;
+
+public class Login extends AppCompatActivity {
+    private Button LoginButton ;
+    private EditText login_Username, login_Password ;
+
+    private ProgressDialog progressDialog;
+    SharedPreferences shpref;
+    SharedPreferences.Editor edit;
+
     @Override
-    public void onCreate(Bundle savedinstantstate)
-    {
-        super.onCreate(savedinstantstate);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        shpref = getSharedPreferences("sh", MODE_PRIVATE);
+        edit = shpref.edit();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        LoginButton = (Button) findViewById(R.id.LoginButton );
+        login_Username= (EditText) findViewById(R.id.login_Username);
+        login_Password = (EditText) findViewById(R.id.login_Password );
+
+
+
+
+        LoginButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String  login_Username_value = login_Username.getText().toString();
+                String login_Password_value = login_Password.getText().toString();
+                checkLogin( login_Username_value , login_Password_value );
+            }
+        });
     }
     public void register(View view)
     {
-        Intent register=new Intent(Login.this,Register.class);
+        Intent register = new Intent(Login.this,Register.class);
         Login.this.startActivity(register);
     }
-    public void login(View view)
-    {
-        EditText login_Username = (EditText) findViewById(R.id.login_Username);
-        EditText login_Password = (EditText) findViewById(R.id.login_Password);
-        final String login_Username_value = login_Username.getText().toString();
-        final String login_Password_value = login_Password.getText().toString();
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://192.168.1.5/user_login.php";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Log.d("Response is: ",response.toString());
+    private void checkLogin(final String login_Username_value , final String login_Password_value ) {
+        String tag_string_req = "req_login";
+        progressDialog.setMessage("Logging in ...");
+        showDialog();
+        StringRequest strReq = new StringRequest(Request.Method.POST,
+                AppURLs.URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                hideDialog();
+                try {
+                    JSONObject jObj = new JSONObject(response);
+                    Log.i("response login", ""+response);
+                    if(jObj.getString("error").equalsIgnoreCase("true"))
+                    {
+                        Toast.makeText(getApplication(), ""+jObj.getString("error_msg"), Toast.LENGTH_SHORT).show();
                     }
-                }, new Response.ErrorListener() {
+                    else
+                    {
+                        edit.putString("token", "login");
+                        edit.putString("id", ""+jObj.getString("Id"));
+                        edit.putString("user", ""+jObj.getString("User"));
+                        edit.commit();
+                        Intent intent = new Intent(Login.this,
+                                Register.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.d("Error : ",error.toString());
+                hideDialog();
             }
-        })
-        {
+        }) {
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError
-            {
+            protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("login_Username", login_Username_value);
-                params.put("login_Password", login_Password_value);
+                params.put("tag", "login");
+                params.put("user", login_Username_value );
+                params.put("password", login_Password_value );
                 return params;
             }
         };
-        queue.add(stringRequest);
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing())
+            progressDialog.show();
+    }
+
+    private void hideDialog() {
+        if (progressDialog.isShowing())
+            progressDialog.dismiss();
     }
 }
 
