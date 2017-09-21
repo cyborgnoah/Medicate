bc5c6d9677ef52fa3faaaae2ccf089a5727da6eb
Pranav Gupta <guptapranav360@gmail.com>
2017-09-21 08:38:34 +0530

    App Download

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Medicate - A Complete Healcare Solution</title>
+<meta name="description" content="This is a complete healthcare solution for the one's who are deeply concerned about their health.">
+<meta name="keywords" content="Health, Healthcare, Solution, Healthcare Solution, Medicate">
+</head>
+<body>
+  <?php
+  header("Location: http://localhost/APK/Medicate.apk");
+  ?>
+</body>
+</html>
