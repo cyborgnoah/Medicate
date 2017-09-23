97ac37da5613c6323131e001bf809641d83c317b
Pranjal Chauhan <pvishuchauhan@gmail.com>
2017-09-23 14:26:59 +0530

    drawer

---
+++ app/src/main/java/com/medical/medicate/per_detail.java
@@ -0,0 +1,14 @@
+package com.medical.medicate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class per_detail extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_per_detail);
+    }
+}
