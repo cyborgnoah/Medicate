3a2e33939c56c813018688c3aa0990477e561bb9
Pranav Gupta <guptapranav360@gmail.com>
2017-10-06 11:21:19 +0530

    Test Update

--- Server/Check.php
+++ Server/Check.php
@@ -1,27 +1,28 @@
 <?php
+$username=$_GET['username'];
+$email=$_GET['email'];
 $hash = md5( rand(0,1000) );
 $sql = "UPDATE userinfo set hash='$hash' where email='$email'";
-if ($conn->query($sql) === TRUE)
+if ($conn->query($sql) == TRUE)
 {
- //echo "Hash has been generated";
+  $to      = $email; // Send email to our user
+  $subject = 'Verify your Account';
+  $headers = 'Medicate a complete care' . "\r\n";
+  $message = '
+
+  Thanks for registering with MEDICATE.Your account has been made. You can login with the following credentials after you have activated your account by pressing the url below.
+
+  ------------------------
+  Username: '.$username.'
+  ------------------------
+
+  Please click this link or copy this link to your browser to activate your account:
+  https://104.131.88.175/Server/validate.php?email='.$email.'&hash='.$hash;
+  mail($to, $subject, $message, $headers); 
 }
 else
 {
  //echo "Error: " . $sql . "<br>" . $conn->error;
 }
-$to      = $email; // Send email to our user
-$subject = 'Verify your Account';
-$headers = 'Medicate a complete care' . "\r\n";
-$message = '
-
-Thanks for registering with MEDICATE.Your account has been made. You can login with the following credentials after you have activated your account by pressing the url below.
-
-------------------------
-Username: '.$username.'
-------------------------
-
-Please click this link or copy this link to your browser to activate your account:
-https://104.131.88.175/Server/validate.php?email='.$email.'&hash='.$hash;
-mail($to, $subject, $message, $headers);
 return(NULL);
 ?>
