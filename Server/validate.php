574684e9d1302e5b11e01d2aca247ebeb1576954
Pranav Gupta <guptapranav360@gmail.com>
2017-10-06 12:51:03 +0530

    Validate Update

--- Server/validate.php
+++ Server/validate.php
@@ -9,7 +9,7 @@ $sql = "SELECT email, hash FROM userinfo WHERE email='".$email."' AND hash='".$h
 $result = mysqli_query($this->db->con,$sql) or die(mysqli_error($this->db->con));
 if ($result->num_rows == 1)
 	{
-		$sql="UPDATE userinfo SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'");
+		$sql="UPDATE userinfo SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'";
 		mysqli_query($this->db->con,$sql) or die(mysqli_error($this->db->con));
 	}
 	else
