32dd1d632b3503d6460f8dffbaef7cdf88274488
Pranav Gupta <guptapranav360@gmail.com>
2017-10-06 13:24:16 +0530

    Mail Update

--- Server/Mail_SendVerificationMail.php
+++ Server/Mail_SendVerificationMail.php
@@ -25,7 +25,7 @@
       Please click this link or copy this link to your browser to activate your account:
       http://104.131.88.175/Server/Mail_ValidateURL.php?email='.$email.'&hash='.$hash;
       mail($to, $subject, $message, $headers);
-      echo "True"
+      echo "True";
     }
     else
     {
