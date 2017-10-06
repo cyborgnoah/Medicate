17f39c0ee85af99bea0a0fb38378265465a7e28a
Pranav Gupta <guptapranav360@gmail.com>
2017-10-06 12:43:26 +0530

    Validate Update

--- Server/validate.php
+++ Server/validate.php
@@ -1,17 +1,19 @@
 <?php
-		require_once 'DB_Functions.php';
-		$email=$_GET['email'];
-		$hash=$_GET['hash'];
-		$sql = "SELECT email, hash FROM userinfo WHERE email='".$email."' AND hash='".$hash."' AND active='0'";
-		$result = $conn->query($sql);	
-		if ($result->num_rows == 1)
-		{
-			$conn->query("UPDATE userinfo SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'");
-			
-		}
-		else
-		{
-					
-		}
-	
-	?>
+error_reporting(E_ALL ^ E_DEPRECATED);
+require_once 'DB_Connect.php';
+$this->db = new DB_Connect();
+$this->db->connect();
+$email=$_GET['email'];
+$hash=$_GET['hash'];
+$sql = "SELECT email, hash FROM userinfo WHERE email='".$email."' AND hash='".$hash."' AND active='0'";
+$result = mysqli_query($this->db->con,$sql) or die(mysqli_error($this->db->con));
+if ($result->num_rows == 1)
+	{
+		$sql="UPDATE userinfo SET active='1' WHERE email='".$email."' AND hash='".$hash."' AND active='0'");
+		mysqli_query($this->db->con,$sql) or die(mysqli_error($this->db->con));
+	}
+	else
+	{
+
+	}
+?>
