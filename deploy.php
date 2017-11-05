6a30ab992ce05c8312c941e8195ebc61cdc795f5
cyborgnoah <guptapranav360@gmail.com>
2017-11-05 07:59:00 +0530

    PHP and APK deleted

--- deploy.php
+++
@@ -1,9 +0,0 @@
-<?php
-// just to automate and sync master branch with digital-diary.tk domain
-$output1=`git reset --hard HEAD`;
-echo"$output1";
-$output2=`git pull 2>&1`;
-echo"$output2";
-$output2=`chmod 777 -R *`;
-echo"$output2";
-?>
