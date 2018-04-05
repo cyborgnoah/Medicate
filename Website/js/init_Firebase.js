977e55c5f7e9a9393b1ee985470164d4a36bdf7e
Pranav Gupta <guptapranav360@gmail.com>
2018-04-05 21:12:20 +0530

    Update

--- Website/js/init_Firebase.js
+++ Website/js/init_Firebase.js
@@ -4,7 +4,7 @@ var app;
 var config = {
   apiKey: "AIzaSyB0BwBC6aiv2oB4d-esbkPiRxHvzumFYOY",
   authDomain: "medicate-c8086.firebaseapp.com",
-  databaseURL: "https://medicate-c8086.firebaseio.com",
+  databaseURL: "https://medicate-c8086.firebaseio.com/",
   projectId: "medicate-c8086",
   storageBucket: "medicate-c8086.appspot.com",
   messagingSenderId: "71702109131"
